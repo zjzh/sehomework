e6da1e7080579ac7fed2d0c1e8b1d039104a3aff
zzj <zejunzh@163.com>
2019-11-08 20:32:24 +0800

    Update OutputStream_write_UnsafePointer_Int.swift

--- MG1733000/swift/OutputStream_write_UnsafePointer_Int.swift
+++ MG1733000/swift/OutputStream_write_UnsafePointer_Int.swift
@@ -1,4 +1,4 @@
-﻿/**类名$方法名(_形参类型)*
+﻿/**类名_方法名(_形参类型)*
  * @author: zhangzejun
  *  @className: OutputStream_write_UnsafePointer_Int
  *  @apiSignature: OutputStream$func write(_ buffer: UnsafePointer<UInt8>, maxLength len: Int) -> Int
