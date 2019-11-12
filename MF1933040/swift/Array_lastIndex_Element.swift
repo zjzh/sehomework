fd2438a01890e731bd6101cd33c7ea8f9ff7a0ac
Sunny <1552917447@qq.com>
2019-11-12 22:54:06 +0800

    Create Array_lastIndex_Element.swift

---
+++ MF1933040/swift/Array_lastIndex_Element.swift
@@ -0,0 +1,41 @@
+/**类名_方法名(_形参类型)*
+ * @author: MF1933040贾晓玉
+ *  @className: Array_lastIndex_Element
+ *  @apiSignature: Array$lastIndex(of element: Element) -> Int?
+ *  @description: Test swift api Array$lastIndex(of element: Element) -> Int?
+ *  @Map: java.util.ArrayList$public int lastIndexOf(object o)
+ */
+import Foundation
+
+class Array_lastIndex_Element{
+     /**
+     * input: 2
+     * class0 class0=["hello", "world"]
+     * var Element = "hello"
+     * output: 1
+     * ret0  ret0=0
+     */
+    static func lastIndex0(){
+        var class0 = ["hello", "world"]
+        var Element = "hello"
+        let ret0=class0.lastIndex(of: Element);
+        assert (ret0==0);
+        print(ret0);
+    }
+    /**
+     * input: 2
+     * class0 class0=["hello", "world"]
+     * var Element = "who"
+     * output: 1
+     * ret0 ret0=nil
+     */
+    static func lastIndex1(){
+        var class0=["hello", "world"];
+        var Element="who";
+        let ret0=class0.lastIndex(of: Element);
+        assert (ret0==nil);
+        print(ret0);
+    }
+}
+Array_lastIndex_Element.lastIndex0()
+Array_lastIndex_Element.lastIndex1()
