d94bd1ef8d6b6ed1d5dea8e1ea21471e954dbe35
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-13 13:47:32 +0800

    Update Int_signum.swift

--- MF1933098/swift/Int_signum.swift
+++ MF1933098/swift/Int_signum.swift
@@ -6,48 +6,48 @@
  * @Map: ava.lang.Integer$public static int signum (int i)
  */
  class Int_signum {
- 	/**
+    /**
      * input: 1
      * class0 class0=2
      * output: 1
      * ret0 ret0=1
      */
-	 static func signum0()
-	 {
-		print(">>>>>>>>>>>>")
-		let class0=2
-		let ret0=class0.signum()
-		assert(ret0==1)
-		print(ret0)
-	 }
- 	/**
+     static func signum0()
+     {
+	     print(">>>>>>>>>>>>")
+	     let class0=2
+	     let ret0=class0.signum()
+	     assert(ret0==1)
+             print(ret0)
+     }
+    /**
      * input: 1
      * class0 class0=-2
      * output: 1
      * ret0 ret0=-1
      */
-	 static func signum1()
-	 {
-		print(">>>>>>>>>>>>")
-		let class0= -2
-		let ret0=class0.signum()
-		assert(ret0== -1) 
-		print(ret0)
-	 }
-	  	/**
+     static func signum1()
+     {
+	     print(">>>>>>>>>>>>")
+	     let class0= -2
+	     let ret0=class0.signum()
+             assert(ret0== -1) 
+	     print(ret0)
+     }
+    /**
      * input: 1
      * class0 class0=0
      * output: 1
      * ret0 ret0=0
      */
-	 static func signum2()
-	 {
-		print(">>>>>>>>>>>>")
-		let class0=0
-		let ret0=class0.signum()
-		assert(ret0==0)
-		print(ret0)
-	 }
+     static func signum2()
+     {
+	     print(">>>>>>>>>>>>")
+	     let class0=0
+	     let ret0=class0.signum()
+	     assert(ret0==0)
+	     print(ret0)
+      }
  }
  Int_signum.signum0()
  Int_signum.signum1()
