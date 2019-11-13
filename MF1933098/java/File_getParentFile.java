71b2682a693db7987b93792b5c90fa14c6eb15cc
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-13 13:40:09 +0800

    Update File_getParentFile.java

--- MF1933098/java/File_getParentFile.java
+++ MF1933098/java/File_getParentFile.java
@@ -9,7 +9,7 @@
 import java.io.File;
  
 public class File_getParentFile {
-     /**
+    /**
      * input: 1
      * class0 File$class0
      * output: 1
@@ -19,26 +19,26 @@ public class File_getParentFile {
     public  static void getParentFile0(){
         System.out.println(">>>>>>>>>>>>");
         File class0=new File("D:/Software/Notepad/test.txt");
-		File ret0=class0.getParentFile();
+	File ret0=class0.getParentFile();
         assert (ret0.getPath()=="D:/Software/Notepad");
         System.out.println(ret0.getPath());
 		
     }
-     /**
+    /**
      * input: 1
      * class0 File$class0
      * output: 1
      * ret0  ret0=false
      * File$class0-File(String pathname) & pathname="**&&%%￥#" & https://developer.android.google.cn/reference/java/io/File.html
      */
-	public  static void getParentFile1(){
+    public  static void getParentFile1(){
         System.out.println(">>>>>>>>>>>>");
         File class0=new File("**&&%%￥#");
-		File ret0=class0.getParentFile();
+	File ret0=class0.getParentFile();
         assert (ret0==null);
 		
     }
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         File_getParentFile.getParentFile0();
         File_getParentFile.getParentFile1();
 
