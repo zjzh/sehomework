cf7e93e4cea12e0fac66ab6376ff26346915d15d
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-13 13:52:41 +0800

    Update URLResourceValues_isExecutable.swift

--- MF1933098/swift/URLResourceValues_isExecutable.swift
+++ MF1933098/swift/URLResourceValues_isExecutable.swift
@@ -6,36 +6,36 @@
  * @Map: java.io.File$public boolean canExecute ()
  */
  import Foundation
- 
+
  class URLResourceValues_isExecutable {
-	/**
+    /**
      * input: 1
      * class0 URLResourceValues$class0
      * output: 1
      * ret0  ret0=true
-	 * URLResourceValues$class0-URL(fileURLWithPath: String) & fileURLWithPath="D:/Software/Notepad/test.txt" & https://developer.apple.com/documentation/foundation/url
-	 */
-	 static func isExecutable0()
-	 {
-		print(">>>>>>>>>>>>")
-		let class0=URL(fileURLWithPath: "D:/Software/Notepad/test.txt")
-		let ret0=try?class0.resourceValues(forKeys:[.isExecutableKey]).isExecutable
-		assert(ret0==true)
-	 }
-	/**
+     * URLResourceValues$class0-URL(fileURLWithPath: String) & fileURLWithPath="D:/Software/Notepad/test.txt" & https://developer.apple.com/documentation/foundation/url
+     */
+    static func isExecutable0()
+    {
+	    print(">>>>>>>>>>>>")
+	    let class0=URL(fileURLWithPath: "D:/Software/Notepad/test.txt")
+	    let ret0=try?class0.resourceValues(forKeys:[.isExecutableKey]).isExecutable
+	    assert(ret0==true)
+    }
+    /**
      * input: 1
      * class0 URLResourceValues$class0
      * output: 1
      * ret0  ret0=false
-	 * URLResourceValues$class0-(fileURLWithPath: String) & fileURLWithPath="**&&%%￥#" & https://developer.apple.com/documentation/foundation/url
-	 */
-	 static func isExecutable1()
-	 {
-		print(">>>>>>>>>>>>")
-		let class0=URL(fileURLWithPath: "**&&%%￥#")
-		let ret0=try?class0.resourceValues(forKeys:[.isExecutableKey]).isExecutable
-		assert(ret0==false)
-	 }
+     * URLResourceValues$class0-(fileURLWithPath: String) & fileURLWithPath="**&&%%￥#" & https://developer.apple.com/documentation/foundation/url
+     */
+    static func isExecutable1()
+    {
+	    print(">>>>>>>>>>>>")
+	    let class0=URL(fileURLWithPath: "**&&%%￥#")
+	    let ret0=try?class0.resourceValues(forKeys:[.isExecutableKey]).isExecutable
+	    assert(ret0==false)
+     }
  }
  URLResourceValues_isExecutable.isExecutable0()
  URLResourceValues_isExecutable.isExecutable1()
