4fff535dcfab8ed27b48e506f0bb155e728636e8
limchallenge <48586027+limchallenge@users.noreply.github.com>
2019-11-13 09:43:45 +0800

    Create Integer_reverseBytes_int.java

---
+++ MF1933105/java/Integer_reverseBytes_int.java
@@ -0,0 +1,43 @@
+/**类名_方法名(_形参类型)
+ * @author: mf1933105xiejiyu
+ *  @className: Integer_reverseBytes_int
+ *  @apiSignature: java.lang.Integer$public static int reverseBytes(int i)
+ *  @description: Test java api java.lang.Integer.reverseBytes(int i)
+ *  @Map: don't have mappings
+ */
+    public class Integer_reverseBytes_int {
+    /**
+     * input: 1
+     * int i=1
+     * output: 1
+     * ret0  ret0=16777216
+     */
+    public  static void reverseBytes0(){
+        System.out.println(">>>>>>>>>>>>");
+        int i=1;
+        int ret0=reverseBytes(i);
+        assert (i==1);
+        assert (ret0==16777216);
+        System.out.println(ret0);
+
+    }
+    /**
+     * input: 1
+     * int i=-1
+     * output: 1
+     * ret0 ret0=-1
+     */
+    public  static void reverseBytes1(){
+        System.out.println(">>>>>>>>>>>>");
+        int i=-1;
+        int ret0=reverseBytes(i);
+        assert (i==-1);
+        assert (ret0==-1);
+        System.out.println(ret0);
+    }
+    public static void main(int[] args) {
+        Integer_reverseBytes_int.reverseBytes0();
+        Integer_reverseBytes_int.reverseBytes1();
+
+    }
+}
