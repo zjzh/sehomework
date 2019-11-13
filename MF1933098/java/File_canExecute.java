566fcf96c5ad74ed36de27e9b7ec1467f50304ef
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-13 13:35:29 +0800

    Update File_canExecute.java

--- MF1933098/java/File_canExecute.java
+++ MF1933098/java/File_canExecute.java
@@ -9,13 +9,13 @@
 import java.io.File;
 
 public class File_canExecute {
-	/**
+    /**
      * input: 1
      * class0 File$class0
      * output: 1
      * ret0  ret0=true
-	 * File$class0-File(String pathname) & pathname="D:/Software/Notepad/test.txt" & https://developer.android.google.cn/reference/java/io/File.html
-	 */
+     * File$class0-File(String pathname) & pathname="D:/Software/Notepad/test.txt" & https://developer.android.google.cn/reference/java/io/File.html
+     */
     public  static void canExecute0(){
         System.out.println(">>>>>>>>>>>>");
         File class0= new File("D:/Software/Notepad/test.txt");
@@ -24,22 +24,22 @@ public class File_canExecute {
         System.out.println(ret0);
 		
     }
-	/**
+    /**
      * input: 1
      * class0 File$class0
      * output: 1
      * ret0  ret0=false
-	 * File$class0-File(String pathname) & pathname="**&&%%￥#" & https://developer.android.google.cn/reference/java/io/File.html
-	 */
-	public  static void canExecute1(){
+     * File$class0-File(String pathname) & pathname="**&&%%￥#" & https://developer.android.google.cn/reference/java/io/File.html
+     */
+    public  static void canExecute1(){
         System.out.println(">>>>>>>>>>>>");
         File class0=new File("**&&%%￥#");
-		boolean ret0=class0.canExecute();
+	boolean ret0=class0.canExecute();
         assert (ret0==false);
         System.out.println(ret0);
 		
     }
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         File_canExecute.canExecute0();
         File_canExecute.canExecute1();
 
