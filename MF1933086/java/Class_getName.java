82995b06a6b1deb6d4398eb7bd9c80f8fa2e35d8
yztongzhan <tongzhan@smail.nju.edu.cn>
2019-11-14 00:22:13 +0800

    修改getName

--- MF1933086/java/Class_getName.java
+++ MF1933086/java/Class_getName.java
@@ -1,12 +1,12 @@
 /*
  * @author: MF1933086 TongZhan
- * @className: Class_getName_
+ * @className: Class_getName
  * @apiSignature: java.lang.Class$public  public String getName()
  * @description: Test Java api java.lang.Class.getName()
  * @Map: Objective-CRuntime$func object_getClassName(_ obj: Any?) -> UnsafePointer<Int8>
  */
 import java.lang.Class;
-public class Class_getName_ {
+public class Class_getName {
 
     /**
      * input: 1
@@ -16,14 +16,14 @@ public class Class_getName_ {
      */
     public static void getName1() {
         System.out.println(">>>>>>>>>>");
-        Class_getName_  class0 = new Class_getName_();
+        Class_getName  class0 = new Class_getName();
         String ret0 = class0.getClass().getName();
-        assert(ret0 == "Class_getName_");
+        assert(ret0 == "Class_getName");
         System.out.println(ret0);
     }
     
     public  static void main (String[] args){
-        Class_getName_.getName1();
+        Class_getName.getName1();
     }
 }
 
