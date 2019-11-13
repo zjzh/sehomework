11df9077149f961f16af2f28bded50457dabdc8a
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-13 13:51:08 +0800

    Update String_lastIndex_function.swift

--- MF1933098/swift/String_lastIndex_function.swift
+++ MF1933098/swift/String_lastIndex_function.swift
@@ -7,39 +7,39 @@
  */
  
  class String_lastIndex_function {
- 	/**
+    /**
      * input: 2
      * class0 class0="hello"
      * wherePre function$wherePre
      * output: 1
      * ret0  String.Index$ret0
-	 * function$wherePre-String$func hasPrefix(_ prefix: String) -> Bool & _="e" & https://developer.apple.com/documentation/swift/string/1540100-hasprefix
+     * function$wherePre-String$func hasPrefix(_ prefix: String) -> Bool & _="e" & https://developer.apple.com/documentation/swift/string/1540100-hasprefix
      */
-	 static func lastIndex0()
-	 {
-		print(">>>>>>>>>>>>")
-		let class0="hello"
-		let wherePre=Array(class0).hasPrefix("e")
-		let ret0=class0.lastIndex(where: wherePre)
-		assert(class0[ret0!]=="e")
-		print(class0[ret0!])
-	 }
- 	/**
+     static func lastIndex0()
+     {
+	    print(">>>>>>>>>>>>")
+	    let class0="hello"
+	    let wherePre=Array(class0).hasPrefix("e")
+	    let ret0=class0.lastIndex(where: wherePre)
+	    assert(class0[ret0!]=="e")
+	    print(class0[ret0!])
+      }
+    /**
      * input: 2
      * class0 class0="hello"
      * wherePre function$wherePre
      * output: 1
      * ret0  String.Index$ret0
-	 * function$wherePre-String$func hasPrefix(_ prefix: String) -> Bool & _="a" & https://developer.apple.com/documentation/swift/string/1540100-hasprefix
+     * function$wherePre-String$func hasPrefix(_ prefix: String) -> Bool & _="a" & https://developer.apple.com/documentation/swift/string/1540100-hasprefix
      */
-	 static func lastIndex1()
-	 {
-		print(">>>>>>>>>>>>")
-		let class0="hello"
-		let wherePre=Array(class0).hasPrefix("a")
-		let ret0=class0.lastIndex(where: wherePre)
-		assert(ret0==nil)
-	 }
+     static func lastIndex1()
+     {
+	    print(">>>>>>>>>>>>")
+	    let class0="hello"
+	    let wherePre=Array(class0).hasPrefix("a")
+	    let ret0=class0.lastIndex(where: wherePre)
+	    assert(ret0==nil)
+     }
  }
  String_lastIndex_function.lastIndex0()
  String_lastIndex_function.lastIndex1()
