94e11d7e665e01f1a8c2257771644161a199e411
黄大头 <1045167276@qq.com>
2019-11-13 20:01:38 -0600

    Add files via upload

---
+++ MF1933038/java/File_isFile.java
@@ -0,0 +1,44 @@
+/**类名_方法名(_形参类型)*
+ * @author:MF1933038黄智衡
+ * @className:File_isFile
+ * @apiSignature:java.io.File$ Boolean isFile()
+ * @description:Test java api java.io.File.isFile()
+ * @Map: FileWrapper$ var isRegularFile: Bool { get }
+ */
+
+ import java.io.File;
+
+ public class File_isFile{
+    /**
+     * input: 1
+     * class0  class0 = new File("F:/")
+     * output: 1
+     * ret  ret0 = false
+     */
+    public static void is_file0(){
+        System.out.println(">>>>>>>>>>>>>>>");
+        File class0 = new File("F:/");
+        boolean ret0 = class0.isFile();
+        String path = class0.getPath();
+        assert(!ret0);
+        System.out.println(path + " is file?\t" + ret0);
+    }
+    /**
+     * input: 1
+     * class0  class0 = new File("F:/test.txt")
+     * output: 1
+     * ret0  ret0 = true
+     */
+    public static void is_file1(){
+        System.out.println(">>>>>>>>>>>>>>>");
+        File class0 = new File("F:/test.txt");
+        boolean ret0 = class0.isFile();
+        String path = class0.getPath();
+        assert(ret0);
+        System.out.println(path + " is file?\t" + ret0);
+    }
+    public static void main(String[] args){
+        is_file0();
+        is_file1();
+    }
+ }
