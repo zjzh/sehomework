42021547bbc399837dae4f57d262a6d041afa493
okJiang <819421878@qq.com>
2019-11-15 14:00:24 +0800

    4th finished
    Collections,146 upload Collections,146 A,B,C,D,E,F

---
+++ MF1933043/java/Collections_min.java
@@ -0,0 +1,38 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**类名_方法名(_形参类型)*
+ * @author: jiangxianjie
+ *  @className: Collections_min
+ *  @apiSignature: java.util.Collections$public static <T extends Object & Comparable<? super T>> T min(Collection<? extends T> coll)
+ *  @description: Test java api java.util.Collections$public static <T extends Object & Comparable<? super T>> T min(Collection<? extends T> coll)
+ *  @Map: Data$@warn_unqualified_access func min() -> UInt8?
+ * @Map: Data$@warn_unqualified_access func min(by areInIncreasingOrder: (UInt8, UInt8) throws -> Bool) rethrows -> UInt8?
+ * @Map: ArraySlice$@warn_unqualified_access func min() -> Element?
+ * @Map: ContiguousArray $@warn_unqualified_access func min() -> Element?
+ * @Map: Array@warn_unqualified_access func min() -> Element?
+ */
+public class Collections_min{
+    /**
+     * input: 1
+     * coll LinkedList$coll
+     * output: 1
+     * ret0 ret0=1
+     * LinkedList<Integer>$class0-LinkedList<Integer>$LinkedList<Integer>.add(x) & x=2 & https://blog.csdn.net/gongchuangsu/article/details/51527042
+     */
+    public static void min0(){
+        System.out.println(">>>>>>>>>>>>");
+        LinkedList<Integer> coll = new LinkedList<>();
+
+        coll.add(1);
+        coll.add(2);
+        coll.add(3);
+        Integer ret0 = Collections.min(coll);
+        assert (ret0 == 1);
+        System.out.println(ret0);
+    }
+    public static void main(String[] args) {
+        Collections_min.min0();
+    }
+
+}
---
+++ MF1933043/swift/Data_min.swift
@@ -0,0 +1,25 @@
+/**类名_方法名(_形参类型)
+ * @author: jiangxianjie
+ *  @className: Data_min
+ *  @apiSignature: Data$@warn_unqualified_access func min() -> UInt8?
+ *  @description: Test swift api Array@warn_unqualified_access func min() -> Element?
+ *  @Map: java.util.Collections$public static <T extends Object & Comparable<? super T>> T min(Collection<? extends T> coll)
+ */
+
+import Foundation
+class Data_min{
+     /**
+     * input: 1
+	 * class0 class0=[1,2,3]
+	 * output: 1
+	 * ret0 ret0="123"
+     */
+    static func min0(){
+        let class0 = [1,2,3]
+		let ret0 = class0.min()
+		assert(ret0 == 1)
+		print(ret0)
+    }
+}
+Data_min.min0()
+
