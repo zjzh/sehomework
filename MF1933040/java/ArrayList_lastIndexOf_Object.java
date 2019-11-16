914ae99c74e48b23af5d2a356b3aa6e73740aa2a
Sunny <1552917447@qq.com>
2019-11-16 18:55:50 +0800

    Update ArrayList_lastIndexOf_Object.java

--- MF1933040/java/ArrayList_lastIndexOf_Object.java
+++ MF1933040/java/ArrayList_lastIndexOf_Object.java
@@ -6,19 +6,19 @@
  *  @Map: Array$func lastIndex(of element: Element) -> Int?
  */
 import java.util.ArrayList;
+import java.util.Arrays;
 public class ArrayList_lastIndexOf_Object {
     /**
      * input: 1
-     * class0 class0=["hello", "world"]
+     * class0 class0 = ArrayList$class0
      * object object="hello"
      * output: 1
      * ret0  ret0=0
+     * ArrayList$class0-ArrayList$ArrayList(Collection c) & c = Arrays.asList(new String[] {"hello", "world"}) &https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
      */
     public  static void lastIndexOf0(){
         System.out.println(">>>>>>>>>>>>");
-        ArrayList<String> class0= new ArrayList<String>();
-        class0.add("hello");
-        class0.add("world");
+        ArrayList<String> class0 = new ArrayList<>(Arrays.asList(new String[] {"hello", "world"}));
         System.out.println(class0);
         String object = "hello";
         int ret0 = class0.lastIndexOf(object);
@@ -28,16 +28,15 @@ public class ArrayList_lastIndexOf_Object {
     }
     /**
      * input: 1
-     * class0 class0=["hello", "world"]
+     * class0 class0 = ArrayList$class0
      * object object="who"
      * output: 1
-     * ret0 ret0=-1
+     * ret0  ret0=0
+     * ArrayList$class0-ArrayList$ArrayList(Collection c) & c = Arrays.asList(new String[] {"hello", "world"}) &https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
      */
     public  static void lastIndexOf1(){
     	System.out.println(">>>>>>>>>>>>");
-        ArrayList<String> class0= new ArrayList<String>();
-        class0.add("hello");
-        class0.add("world");
+        ArrayList<String> class0 = new ArrayList<>(Arrays.asList(new String[] {"hello", "world"}));
         System.out.println(class0);
         String object = "who";
         int ret0 = class0.lastIndexOf(object);
