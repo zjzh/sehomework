095b4c7e4bf63548a5e51570821ceaa24b9b5f3e
Sunny <1552917447@qq.com>
2019-11-17 20:10:42 +0800

    Create String_valueOf_Object.java

---
+++ MF1933040/java/String_valueOf_Object.java
@@ -0,0 +1,24 @@
+/**类名_方法名(_形参类型)*
+ * @author: MF1933040贾晓玉
+ *  @className: String_valueOf_Object
+ *  @apiSignature: java.lang.String$public static String valueOf(Object obj)
+ *  @description: Test java api java.lang.String$public static String valueOf(Object obj)
+ *  @Map: String$init<Subject>(describing instance: Subject)
+ */
+public class String_valueOf_Object {
+	/**
+     * input: 1
+     * Object obj = 123
+     * output: 1
+     * ret0 = "123"
+     **/
+	public static void valueOf0() {
+		int obj = 123;
+		String ret0 = String.valueOf(obj);
+		assert(ret0 == "123");
+		System.out.println(ret0);
+	}
+	public static void main(String[] args) {
+		String_valueOf_Object.valueOf0();
+	}
+}
