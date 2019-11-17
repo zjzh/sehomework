c4d2d24dbb3bbac3526f265da2c38feeb9d6f163
Sunny <1552917447@qq.com>
2019-11-17 20:06:57 +0800

    Create Thread_sleep_long.java

---
+++ MF1933040/java/Thread_sleep_long.java
@@ -0,0 +1,30 @@
+/**类名_方法名(_形参类型)*
+ * @author: MF1933040贾晓玉
+ *  @className: Thread_sleep_long 
+ *  @apiSignature: java.lang.Thread$public static void sleep(long millis)
+ *  @description: Test java api java.lang.Thread$public static void sleep(long millis)
+ *  @Map: Thread$func sleep(forTimeInterval ti: TimeInterval)
+ */
+import java.lang.Thread;
+public class Thread_sleep_long {
+	/**
+     * input: 2
+     * class0 class0=Thread$class0
+     * long m = 3000
+     * output: 0
+     * Thread$class0-Thread$new Thread()$https://docs.oracle.com/javase/8/docs/api/java/lang/Thread.html
+     **/
+	public static void sleep0() throws InterruptedException {
+		Thread class0 = new Thread();
+		long m = 4000;
+		long startTime = System.currentTimeMillis();
+		class0.sleep(m);
+		long endTime = System.currentTimeMillis();
+		long timeSpan = endTime - startTime;
+		assert(m == timeSpan);
+		System.out.println(timeSpan);
+	}
+	public static void main(String[] args) throws InterruptedException {
+		Thread_sleep_long.sleep0();
+	}
+}
