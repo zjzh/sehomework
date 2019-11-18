13230c19f6b1d9cf8b7b85f50de84a847988a503
GCCAT <45195841+GCCAT@users.noreply.github.com>
2019-11-18 17:22:51 +0800

    Update NSMapTable_count.swift

--- MF1933116/swift/NSMapTable_count.swift
+++ MF1933116/swift/NSMapTable_count.swift
@@ -18,35 +18,37 @@
 class NSMapTable_count {
 
     /**
-     * input: 0
+     * input: 1
+     * class0 NSMapTable$class0
      * output: 1
      * ret0 ret0 = 3
      */
     static func count0() {
         
-	var table =  NSMapTable<TestClass,NSString>(keyOptions: .strongMemory, valueOptions: .weakMemory)
+	var class0 =  NSMapTable<TestClass,NSString>(keyOptions: .strongMemory, valueOptions: .weakMemory)
         
 	var test0: TestClass? = TestClass(a:"name")
 	var test1: TestClass? = TestClass(a:"age")
 	var test2: TestClass? = TestClass(a:"height")
-	table.setObject(test0, forKey: "1")
-	table.setObject(test1, forKey: "2")
-	table.setObject(test2, forKey: "3")
+	class0.setObject(test0, forKey: "1")
+	class0.setObject(test1, forKey: "2")
+	class0.setObject(test2, forKey: "3")
 
-        var ret0 = table.count
+        var ret0 = class0.count
         assert(ret0 == 3)
         print(ret0)
     }
     /**
-     * input: 0
+     * input: 1
+     * class0 NSMapTable$class0
      * output: 1
      * ret0 ret0 = 0
      */
     static func count1() {
         
-	var table =  NSMapTable<TestClass,NSString>(keyOptions: .strongMemory, valueOptions: .weakMemory)
+	var class0 =  NSMapTable<TestClass,NSString>(keyOptions: .strongMemory, valueOptions: .weakMemory)
 
-        var ret0 = table.count
+        var ret0 = class0.count
         assert(ret0 == 0)
         print(ret0)
     }
