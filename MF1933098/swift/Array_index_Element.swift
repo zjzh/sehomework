1c666a49fa7abf929510d3a2d91757e15abe896b
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-18 20:37:39 +0800

    Update and rename Array_index_Element to Array_index_Element.swift

--- MF1933098/swift/Array_index_Element.swift
+++ MF1933098/swift/Array_index_Element.swift
@@ -7,42 +7,42 @@
  */
  
  class Array_index_Element {
- 	/**
+    /**
      * input: 2
      * class0 class0=["hello","world","hello"]
      * of of="hello"
      * output: 1
      * ret0 0
      */
-	 static func index0()
-	 {
-		print(">>>>>>>>>>>>")
-		var class0=Array<String>()
-		class0.append("hello")
-		class0.append("world")
-		class0.append("hello")
-		let ret0=class0.index(of: "hello")
-		assert(ret0==0)
-		print(ret0)
-	 }
- 	/**
+     static func index0()
+     {
+	print(">>>>>>>>>>>>")
+	var class0=Array<String>()
+	class0.append("hello")
+	class0.append("world")
+	class0.append("hello")
+	let ret0=class0.index(of: "hello")
+	assert(ret0==0)
+	print(ret0)
+     }
+    /**
      * input: 2
      * class0 class0=["hello","world","hello"]
      * of of="wow"
      * output: 1
      * ret0 0
      */
-	 static func index1()
-	 {
-		print(">>>>>>>>>>>>")
-		var class0=Array<String>()
-		class0.append("hello")
-		class0.append("world")
-		class0.append("hello")
-		let ret0=class0.index(of: "wow")
-		assert(ret0==nil)
-		print(ret0)
-	 }
+     static func index1()
+     {
+	print(">>>>>>>>>>>>")
+	var class0=Array<String>()
+	class0.append("hello")
+	class0.append("world")
+	class0.append("hello")
+	let ret0=class0.index(of: "wow")
+	assert(ret0==nil)
+	print(ret0)
+     }
  }
  Array_index_Element.index0()
  Array_index_Element.index1()
