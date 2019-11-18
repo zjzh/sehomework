3f2bfe2ba600894b990188ded578ba36e95870bb
GCCAT <45195841+GCCAT@users.noreply.github.com>
2019-11-18 16:16:02 +0800

    Update Dictionary_count.swift

--- MF1933116/swift/Dictionary_count.swift
+++ MF1933116/swift/Dictionary_count.swift
@@ -7,36 +7,39 @@
 */
 class Dictionary_count{
     /**
-     * input: 0
+     * input: 1
+     * class0 Dictionary$class0
      * output: 1
      * ret0  ret0 = 3
      */
     static func count0(){
-        var someDict0:[Int:String] = [1:"One", 2:"Two", 3:"Three"]
-        var ret0=someDict0.count
+        var class0:[Int:String] = [1:"One", 2:"Two", 3:"Three"]
+        var ret0=class0.count
         assert(ret0 == 3)
         print(ret0)
     }
     /**
-     * input: 0
+     * input: 1
+     * class0 Dictionary$class0
      * output: 1
      * ret0  ret0 = 3
      */
     static func count1(){
-        var someDict0:[Int:String] = [1:"One", 2:"Two", 3:"Three"]
-        someDict0.updateValue("Ones", forKey: 1)
-        var ret0=someDict0.count
+        var class0:[Int:String] = [1:"One", 2:"Two", 3:"Three"]
+        class0.updateValue("Ones", forKey: 1)
+        var ret0=class0.count
         assert(ret0 == 3)
         print(ret0)
     }
     /**
-     * input: 0
+     * input: 1
+     * class0 Dictionary$class0
      * output: 1
      * ret0  ret0 = 0
      */
 	static func count2(){
-        var someDict0:[Int:String] = [:]
-        var ret0=someDict0.count
+        var class0:[Int:String] = [:]
+        var ret0=class0.count
         assert(ret0 == 0)
         print(ret0)
     }
