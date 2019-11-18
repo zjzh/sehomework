d9243f7a30a62964635c8f3027b5801ce24ea1c6
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-18 20:40:15 +0800

    Update and rename ContiguousArray_index_Element to ContiguousArray_index_Element.swift

--- MF1933098/swift/ContiguousArray_index_Element.swift
+++ MF1933098/swift/ContiguousArray_index_Element.swift
@@ -7,42 +7,42 @@
  */
  
  class ContiguousArray_index_Element {
- 	/**
+    /**
      * input: 2
      * class0 class0=["hello","world","hello"]
      * of of="hello"
      * output: 1
      * ret0 0
      */
-	 static func index0()
-	 {
-		print(">>>>>>>>>>>>")
-		var class0=ContiguousArray<String>()
-		class0.append("hello")
-		class0.append("world")
-		class0.append("hello")
-		let ret0=class0.index(of: "hello")
-		assert(ret0==0)
-		print(ret0)
-	 }
- 	/**
+     static func index0()
+     {
+	print(">>>>>>>>>>>>")
+	var class0=ContiguousArray<String>()
+	class0.append("hello")
+	class0.append("world")
+	class0.append("hello")
+	let ret0=class0.index(of: "hello")
+	assert(ret0==0)
+	print(ret0)
+     }
+    /**
      * input: 2
      * class0 class0=["hello","world","hello"]
      * of of="wow"
      * output: 1
      * ret0 0
      */
-	 static func index1()
-	 {
-		print(">>>>>>>>>>>>")
-		var class0=ContiguousArray<String>()
-		class0.append("hello")
-		class0.append("world")
-		class0.append("hello")
-		let ret0=class0.index(of: "wow")
-		assert(ret0==nil)
-		print(ret0)
-	 }
+     static func index1()
+     {
+	print(">>>>>>>>>>>>")
+	var class0=ContiguousArray<String>()
+	class0.append("hello")
+	class0.append("world")
+	class0.append("hello")
+	let ret0=class0.index(of: "wow")
+	assert(ret0==nil)
+	print(ret0)
+     }
  }
  ContiguousArray_index_Element.index0()
  ContiguousArray_index_Element.index1()
