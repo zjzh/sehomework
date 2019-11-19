861446b431053d3c5227adccec03fbe370487bcc
thexjx <thexjx@gmail.com>
2019-11-19 23:16:25 +0800

    Update ContiguousArray_suffix_int__ContiguousArray_makeIterator.swift

--- MG1933072/swift/ContiguousArray_suffix_int__ContiguousArray_makeIterator.swift
+++ MG1933072/swift/ContiguousArray_suffix_int__ContiguousArray_makeIterator.swift
@@ -7,37 +7,65 @@ import Foundation
  * @description: Test swift api ContiguousArray$suffix(_ maxLength: Int) -> ArraySlice<Element>;ContiguousArray$makeIterator() -> IndexingIterator<ContiguousArray<Element>>
  * @Map: ArrayList$public ListIterator<E> listIterator(int index);LinkedList$public ListIterator<E> listIterator(int index)
  */
-class ContiguousArray_suffix_int_ContiguousArray_makeIterator{
+class ContiguousArray_suffix_int__ContiguousArray_makeIterator{
 	/**
      * input: 1
      * class0 ContiguousArray$class0
      * output: 1
      * ret0 ret0="[4, 5]\nIndexingIterator<ArraySlice<Int>>(_elements: ArraySlice([4, 5]), _position: 3)"
      */
-	static func suffix_makeIterator0(){
-		let class0 = ContiguousArray<Int>([1,2,3,4,5])
 
-		let suffixArr = class0.suffix(from: 3)
-		print(suffixArr)
-		let iter = suffixArr.makeIterator()
-		print(iter)
+     /**
+     * input: 2
+     * class0 class0= [1,2,3,4,5]
+     * _ = 3
+     * output: 2
+     # ret0 ret0= [4,5]
+     * ret1 IndexingIterator$ret1
+     * IndexingIterator$ret1-IndexingIterator();ContiguousArray$makeIterator() & https://developer.apple.com/documentation/swift/contiguousarray
+     */
+
+	static func suffix_makeIterator0(){
+		print(">>>>>>>>")
+		let class0 = ContiguousArray<Int>([1,2,3,4,5])
+		let ret0 = class0.suffix(from: 3)
+		var ret1 = ret0.makeIterator()
+		let nextElement = ret1.next()
+		
+		assert(ret0 == [4,5])
+		assert(nextElement == 4)
+		
+		print("suffix: ", terminator: "")
+		print(ret0)
+		print("nextElement: ", terminator: "")
+		print(nextElement)
 	}
 
 	/**
-     * input: 1
-     * class0 ContiguousArray$class0
-     * output: 1
-     * ret0 ret0="["N", "J", "U"]\nIndexingIterator<ArraySlice<String>>(_elements: ArraySlice(["N", "J", "U"]), _position: 5)"
+     * input: 2
+     * class0 class0= [ "I","L","o","v","e","N","J","U"]
+     * _ = 3
+     * output: 2
+     # ret0 ret0= ["N","J","U"]
+     * ret1 IndexingIterator$ret1
+     * IndexingIterator$ret1-IndexingIterator();ContiguousArray$makeIterator() & https://developer.apple.com/documentation/swift/contiguousarray
      */
 	static func suffix_makeIterator1(){
+		print(">>>>>>>>")
 		let class0 = ContiguousArray<String>([ "I","L","o","v","e","N","J","U"])
-
-		let suffixArr = class0.suffix(from: 5)
-		print(suffixArr)
-		let iter = suffixArr.makeIterator()
-		print(iter)
+		let ret0 = class0.suffix(from: 5)
+		var ret1 = ret0.makeIterator()
+		let nextElement = ret1.next()
+		
+		assert(ret0 == ["N","J","U"])
+		assert(nextElement == "N")
+		
+		print("suffix: ", terminator: "")
+		print(ret0)
+		print("nextElement: ", terminator: "")
+		print(nextElement)
 	}
 }
 
-ContiguousArray_suffix_int_ContiguousArray_makeIterator.suffix_makeIterator0()
-ContiguousArray_suffix_int_ContiguousArray_makeIterator.suffix_makeIterator1()
+ContiguousArray_suffix_int__ContiguousArray_makeIterator.suffix_makeIterator0()
+ContiguousArray_suffix_int__ContiguousArray_makeIterator.suffix_makeIterator1()
