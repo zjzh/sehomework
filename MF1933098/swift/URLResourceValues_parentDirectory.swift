734d32b335b573747bc1f882703f3fa0a482ea9c
wangyue97 <32741013+wangyue97@users.noreply.github.com>
2019-11-20 14:43:53 +0800

    Add files via upload

---
+++ MF1933098/swift/URLResourceValues_parentDirectory.swift
@@ -0,0 +1,42 @@
+﻿/**类名_方法名(_形参类型)
+ * @author: MF1933098王悦
+ * @className: URLResourceValues_parentDirectory
+ * @apiSignature: URLResourceValues$var parentDirectory: URL? { get }
+ * @description: Test swift api URLResourceValues$var parentDirectory: URL? { get }
+ * @Map: java.io.File$public File getParentFile ()
+ */
+ 
+ import Foundation
+ 
+ class URLResourceValues_parentDirectory {
+   /**
+     * input: 1
+     * class0 URLResourceValues$class0
+     * output: 1
+     * ret0 URLResourceValues$ret0
+     * URLResourceValues$class0-(fileURLWithPath: String) & fileURLWithPath="D:/Software/Notepad/test.txt" & https://developer.apple.com/documentation/foundation/url
+     */
+     static func parentDirectory0()
+     {
+	print(">>>>>>>>>>>>")
+	let class0=URL(fileURLWithPath: "D:/Software/Notepad/test.txt")
+	let ret0=try?class0.resourceValues(forKeys:[.parentDirectoryURLKey]).parentDirectory
+	assert(ret0==URL(fileURLWithPath: "D:/Software/Notepad"))
+     }
+   /**
+     * input: 1
+     * class0 URLResourceValues$class0
+     * output: 1
+     * ret0 URLResourceValues$ret0
+     * URLResourceValues$class0-(fileURLWithPath: String) & fileURLWithPath="**&&%%￥#" & https://developer.apple.com/documentation/foundation/url
+     */
+     static func parentDirectory1()
+     {
+	print(">>>>>>>>>>>>")
+	let class0=URL(fileURLWithPath: "**&&%%￥#")
+	let ret0=try?class0.resourceValues(forKeys:[.parentDirectoryURLKey]).parentDirectory
+	assert(ret0==nil)
+     }
+ }
+ URLResourceValues_parentDirectory.parentDirectory0()
+ URLResourceValues_parentDirectory.parentDirectory1()
