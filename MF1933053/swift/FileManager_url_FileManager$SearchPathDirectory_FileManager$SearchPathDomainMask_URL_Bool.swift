490f28054cc13aa61a8e30387c1132f9c1e22082
TonyLee12 <1872699882@qq.com>
2019-11-21 14:16:29 +0800

    filemanager.url

---
+++ MF1933053/swift/FileManager_url_FileManager$SearchPathDirectory_FileManager$SearchPathDomainMask_URL_Bool.swift
@@ -0,0 +1,28 @@
+/**
+* @author: MF1933053李志成
+* @className: FileManager_FileManager$SearchPathDirectory_FileManager$SearchPathDomainMask_URL_Bool
+* @apiSinature: FileManager$func func url(for directory: FileManager.SearchPathDirectory, in domain: FileManager.SearchPathDomainMask, appropriateFor url: URL?, create shouldCreate: Bool) throws -> URL
+* @description: Test swift api FileManager$func url()
+* @Map: java.io.File$public boolean mkdir()
+*/
+import Foundation
+class FileManager_FileManager$SearchPathDirectory_FileManager$SearchPathDomainMask_URL_Bool{
+    /**
+     * input: 3
+	 * class0 FileManager$class0
+     * shouldCreate shouldCreate = true
+	 * url url = URL(string:".")
+     * output: 1
+     * ret0 ret0 = URL
+	 * FileManager$class0- FileManager.default & & https://developer.apple.com/documentation/foundation/filemanager/1407693-url
+    */
+    static func url0(){
+		let url = URL(string:".")
+        let shouldCreate = true
+		let class0 = FileManager.default
+        let ret0 = try?class0.url(for:FileManager.SearchPathDirectory.documentDirectory, in:FileManager.SearchPathDomainMask.userDomainMask, appropriateFor:url, create:shouldCreate)
+        assert(ret0 != nil)
+        print(ret0)
+    }
+}
+FileManager_FileManager$SearchPathDirectory_FileManager$SearchPathDomainMask_URL_Bool.url0()
