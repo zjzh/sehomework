f54b2319302e777bb242d772d1d63cbd9308b77c
thexjx <thexjx@gmail.com>
2019-11-25 11:30:36 +0800

    Update Array_suffix_int__Array_makeIterator.swift

--- MG1933072/swift/Array_suffix_int__Array_makeIterator.swift
+++ MG1933072/swift/Array_suffix_int__Array_makeIterator.swift
@@ -15,7 +15,7 @@ class Array_suffix_int__Array_makeIterator{
      * output: 2
      # ret0 ret0= [4,5]
      * ret1 IndexingIterator$ret1
-     * IndexingIterator$ret1-IndexingIterator();Array$makeIterator() & https://developer.apple.com/documentation/swift/array
+     * IndexingIterator$ret1-Array$makeIterator() & https://developer.apple.com/documentation/swift/array
      */
 	static func suffix_makeIterator0(){
 		print(">>>>>>>>")
@@ -40,7 +40,7 @@ class Array_suffix_int__Array_makeIterator{
      * output: 2
      # ret0 ret0= ["N","J","U"]
      * ret1 IndexingIterator$ret1
-     * IndexingIterator$ret1-IndexingIterator();Array$makeIterator() & https://developer.apple.com/documentation/swift/array
+     * IndexingIterator$ret1-Array$makeIterator() & https://developer.apple.com/documentation/swift/array
      */
 	static func suffix_makeIterator1(){
 		print(">>>>>>>>")
