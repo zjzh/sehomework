555e6a1fd45f145e9985a69a59e63408fd9ec9bc
ZaiXiaochen <54533367+ZaiXiaochen@users.noreply.github.com>
2019-11-27 21:01:05 +0800

    Update Class_getDeclaringClass.java

--- MF1933114/java/Class_getDeclaringClass.java
+++ MF1933114/java/Class_getDeclaringClass.java
@@ -10,14 +10,14 @@ import java.lang.reflect.Method;
 public class Class_getDeclaringClass{
     /**
 	*input:1
-	*class0 Method$m
+	*class0 Method$class0
 	*output:1
 	*ret0 Class$ret0=java.lang.String
-	*Method$m-Class$getDeclaringClass & Method m=String.class.getMethods()[0]; & https://docs.oracle.com/javase/8/docs/api/java/beans/MethodDescriptor.html#getMethod--
+	*Method$class0-Class$getDeclaringClass & Method class0=String.class.getMethods()[0]; & https://docs.oracle.com/javase/8/docs/api/java/beans/MethodDescriptor.html#getMethod--
 	*/
     public static void getDeclaringClass0() {
          
-        Method m=String.class.getMethods()[0];
+        Method class0=String.class.getMethods()[0];
         Class ret0=m.getDeclaringClass();
         assert(ret0==String.class);
         System.out.println(ret0);
