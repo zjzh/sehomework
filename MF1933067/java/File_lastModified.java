66ac7b4129ae06a81d89232a10902f0fd63e9956
Tom_LK <longshenhao@vip.qq.com>
2019-11-27 16:06:42 +0800

    Update File_lastModified.java

--- MF1933067/java/File_lastModified.java
+++ MF1933067/java/File_lastModified.java
@@ -13,7 +13,7 @@ public class File_lastModified {
      * output:1
      * ret0 ret0=millsec  //不确定的，因为与时间有关
      * File$class0-File$File(String pathname) & String$pathname="file.txt" & https://developer.android.com/reference/java/io/File.html?hl=en#File(java.lang.String)
-     * File$class0-File$exists() & https://developer.android.com/reference/java/io/File.html?hl=en#exists()
+     * File$class0-File$exists() & & https://developer.android.com/reference/java/io/File.html?hl=en#exists()
      */
     public static void lastmodifiled0(){
         System.out.println(">>>>>>>>>>>>");
