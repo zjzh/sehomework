7abe1a94e5b5895847a4c317f72828c7113f2079
Sunny <1552917447@qq.com>
2019-11-27 22:18:12 +0800

    Update Calendar_set_int_int_int_int_int_int.java

--- MF1933040/java/Calendar_set_int_int_int_int_int_int.java
+++ MF1933040/java/Calendar_set_int_int_int_int_int_int.java
@@ -2,18 +2,8 @@ package test;
 /**类名_方法名(_形参类型)*
  * @author: MF1933040 贾晓玉
  *  @className: Calendar_set_int_int_int_int_int_int
- *  @apiSignature: java.util.Calendar$public final void set(int year,
- *                     int month,
- *                     int date,
- *                     int hourOfDay,
- *                     int minute,
- *                     int second)
- *  @description: Test java api  java.util.Calendar$public final void set(int year,
- *                     int month,
- *                     int date,
- *                     int hourOfDay,
- *                     int minute,
- *                     int second)
+ *  @apiSignature: java.util.Calendar$public final void set(int year,int month,int date,int hourOfDay，int minute,int second)
+ *  @description: Test java api  java.util.Calendar$public final void set(int year,int month,int date,int hourOfDay,int minute,int second)
  *  @Map:DateComponents$init(calendar: Calendar? = nil, timeZone: TimeZone? = nil, era: Int? = nil, year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil, nanosecond: Int? = nil, weekday: Int? = nil, weekdayOrdinal: Int? = nil, quarter: Int? = nil, weekOfMonth: Int? = nil, weekOfYear: Int? = nil, yearForWeekOfYear: Int? = nil);
  *  NSCalendar$func date(from comps: DateComponents) -> Date?
  */
