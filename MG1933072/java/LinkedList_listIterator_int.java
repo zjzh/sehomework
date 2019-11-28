d167f7178e302407973cb0cae13b3e57590fc185
thexjx <thexjx@gmail.com>
2019-11-28 21:33:52 +0800

    Update LinkedList_listIterator_int.java

--- MG1933072/java/LinkedList_listIterator_int.java
+++ MG1933072/java/LinkedList_listIterator_int.java
@@ -17,7 +17,7 @@ public class LinkedList_listIterator_int {
      * index index=1
      * output: 1
      * ret0 ListIterator$ret0
-     * LinkedList$class0-LinkedList$LinkedList();LinkedList$add(e) & e="I" & https://docs.oracle.com/javase/8/docs/api/java/util/LinkedList.html
+     * LinkedList$class0-LinkedList$LinkedList();LinkedList$add(E e) & e="I" & https://docs.oracle.com/javase/8/docs/api/java/util/LinkedList.html
      * ListIterator$ret0-LinkedList$listIterator(int index) & index=1 & https://docs.oracle.com/javase/8/docs/api/java/util/List.html#listIterator-int-
      */
     public static void listIterator0(){
@@ -41,7 +41,7 @@ public class LinkedList_listIterator_int {
      * index index=2
      * output: 1
      * ret0 ListIterator$ret0
-     * LinkedList$class0-LinkedList$LinkedList();LinkedList$add(e) & e="I" & https://docs.oracle.com/javase/8/docs/api/java/util/LinkedList.html
+     * LinkedList$class0-LinkedList$LinkedList();LinkedList$add(E e) & e="I" & https://docs.oracle.com/javase/8/docs/api/java/util/LinkedList.html
      * ListIterator$ret0-LinkedList$listIterator(int index) & index=2 & https://docs.oracle.com/javase/8/docs/api/java/util/List.html#listIterator-int-
      */
     public static void listIterator1(){
