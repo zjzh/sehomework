9eb84a44f12c9d77550a024d19dd6d44844cb5ef
黄大头 <1045167276@qq.com>
2019-11-28 14:53:11 +0800

    Update Thread_start.swift

--- MF1933038/swift/Thread_start.swift
+++ MF1933038/swift/Thread_start.swift
@@ -1,3 +1,4 @@
+  
 /**类名_方法名(_形参类型)*
  *  @author: MF1933038黄智衡
  *  @className: Thread_start
@@ -8,23 +9,18 @@
 
  import Foundation
 
-class test_thread{
-    @objc func runThread(){
-        for i in 1..<100{
-            print("thread is running..\(i)")
-        }
-    }
-}
-
 class Thread_start{
     /**
      * input: 1
      * class0 Thread$class0
-     * output: NA
-     * Thread$class0-Thread(target: target, selector: selector, object: nil) & target$target = test_thread() & selector$selector = #selector(test_thread.runThread) & https://developer.apple.com/documentation/foundation/thread/
+     * output: 0
+     * Thread$class0-Thread$Thread() & https://developer.apple.com/documentation/foundation/thread/
      */
     static func thread_run(){
-        let class0 = Thread(target: test_thread(), selector: #selector(test_thread.runThread), object: nil)
+        let class0 = Thread(){
+            sleep(2)
+            print("Thread is running...")
+        }
         class0.start()
         print("ok !")
     }
