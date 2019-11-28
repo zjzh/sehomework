3f5788fad7d952adfb9a6e7e9f8c0acabdc65b1c
黄大头 <1045167276@qq.com>
2019-11-28 20:43:36 +0800

    Update Class_getFields.java

--- MF1933038/java/Class_getFields.java
+++ MF1933038/java/Class_getFields.java
@@ -14,7 +14,7 @@ import java.lang.reflect.*;
      * class0  Class$class0
      * output: 1
      * ret0  Field[]$ret0
-     * Class$class0-Class.forName(String className) & String$className = "Test_Man" & https://developer.android.google.cn/reference/java/lang/Class?hl=en
+     * Class$class0-Class$forName(String className) & String$className = "Test_Man" & https://developer.android.google.cn/reference/java/lang/Class?hl=en
      */ 
     public static void Fields_of_Class(){
         System.out.println(">>>>>>>>>>>>>>>");
