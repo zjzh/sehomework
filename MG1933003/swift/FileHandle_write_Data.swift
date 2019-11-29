9515d1bb8955130d741a11521693a42fd0d4e679
ChenLLong <38234820+ChenLLong@users.noreply.github.com>
2019-11-29 18:20:24 +0800

    Update FileHandle_write_Data.swift

--- MG1933003/swift/FileHandle_write_Data.swift
+++ MG1933003/swift/FileHandle_write_Data.swift
@@ -10,22 +10,22 @@ class FileHandle_write_Data{
      /**
      * input: 2
      * class0 FileHandle$class0
-     * buffer buffer= [1,2,3,4,5]
+     * data data= [1,2,3,4,5]
      * output: 0
-	 * FileHandle$class0
-	 * FileHandle$class0-FileHandle$init?(forWritingAtPath path: String) & String$path="file.txt" & https://developer.apple.com/documentation/foundation/filehandle/1414405-init
+     * FileHandle$class0
+     * FileHandle$class0-FileHandle$init?(forWritingAtPath path: String) & String$path="file.txt" & https://developer.apple.com/documentation/foundation/filehandle/1414405-init
      */
     static func write0(){
         let toFileAtPath="file.txt"
         var class0 = FileHandle.init(forWritingAtPath: toFileAtPath)
-        var buffer = Data.init([1,2,3,4,5])
+        var data = Data.init([1,2,3,4,5])
         class0!.write(buffer)
         let inp=InputStream(fileAtPath:toFileAtPath)
         inp?.open()
         var data=[UInt8](repeating: 0, count: 5)
         let a=inp?.read(&data, maxLength: 5)
 		
-		assert(data == [1,2,3,4,5])
+	assert(data == [1,2,3,4,5])
 	  
         print(data)
     }
