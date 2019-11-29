0c1085482201f5795f8c657d6c2976463bb79f88
ChenLLong <38234820+ChenLLong@users.noreply.github.com>
2019-11-29 17:45:52 +0800

    Update FileOutputStream_write_array.java

--- MG1933003/java/FileOutputStream_write_array.java
+++ MG1933003/java/FileOutputStream_write_array.java
@@ -12,15 +12,15 @@ public class FileOutputStream_write_array {
 	/*
 	 * input:2
 	 * class0 FileOutputSteam$class0
-	 * array array = {1,2,3,4,5}
+	 * b b = {1,2,3,4,5}
 	 * output:0
 	 * FileOutputSteam$class0-FileOutputSteam$FileOutputStream(String name) & String$fileName="result.txt" & link-https://docs.oracle.com/javase/9/docs/api/java/io/FileOutputStream.html
 	 */
 	public static void write0(){
 		 try {
 		 	FileOutputStream fos = new FileOutputStream("file.txt");
-	     	byte[] array = {1,2,3,4,5};
-	     	fos.write(array);
+	     		byte[] b = {1,2,3,4,5};
+	     		fos.write(b);
 			 
 		 	FileInputStream in = new FileInputStream("file.txt");
 		 	byte[] content = new byte[10];
@@ -28,7 +28,7 @@ public class FileOutputStream_write_array {
 			
 			assert(contentlen == 5);
 			assert(content[0] == 1);
-		    assert(content[1] == 2);
+		   	assert(content[1] == 2);
 			assert(content[2] == 3);
 			assert(content[3] == 4);
 			assert(content[4] == 5);
