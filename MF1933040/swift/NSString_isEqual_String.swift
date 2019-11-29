4c55c7793bfffdc1bf713742e8ddd67d3d2ceded
Sunny <1552917447@qq.com>
2019-11-29 15:28:15 +0800

    Update NSString_isEqual_String.swift

--- MF1933040/swift/NSString_isEqual_String.swift
+++ MF1933040/swift/NSString_isEqual_String.swift
@@ -11,30 +11,30 @@ class NSString_isEqual_String{
      /**
      * input: 2
      * class0 class0="hello world"
-     * astring astring= "hello world"
+     * aString aString= "hello world"
      * output: 1
      * ret0  ret0=true
      */
     static func isEqual0(){
         let class0 = "hello world"
-        let astring = "hello world"
-        let ret0 = class0.isEqual(astring)
+        let aString = "hello world"
+        let ret0 = class0.isEqual(aString)
         assert(ret0 == true)
-        print(class0.isEqual(astring))
+        print(class0.isEqual(aString))
     }
     /**
     * input: 2
     * class0 class0="hello world"
-    * astring astring= "hello"
+    * aString aString= "hello"
     * output: 1
     * ret0  ret0=false
     */
    static func isEqual1(){
        let class0 = "hello world"
-       let astring = "hello"
-       let ret0 = class0.isEqual(astring)
+       let aString = "hello"
+       let ret0 = class0.isEqual(aString)
        assert(ret0 == false)
-       print(class0.isEqual(astring))
+       print(class0.isEqual(aString))
    }
 }
 NSString_isEqual_String.isEqual0()
